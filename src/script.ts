7ddcdd0a9090226f236dbb20f9e496fb601d2214
Ann <vansuwaris@gmail.com>
2023-04-11 17:00:40 +0530

    Todo-App using TypeScript

---
+++ src/script.ts
@@ -0,0 +1,149 @@
+// Define Todo interface
+interface Todo {
+  id: number;
+  task: string;
+  completed: boolean; // Added 'completed' property
+}
+
+// Select DOM elements
+const formTodo = document.querySelector('.todo-form') as HTMLFormElement;
+const inputTodo = document.querySelector('.todo-input') as HTMLInputElement;
+const btnAddTodo = document.querySelector('.todo-btn-add') as HTMLButtonElement;
+const todoList = document.querySelector('.todo-list') as HTMLUListElement;
+const todoCount = document.querySelector('.todoCount') as HTMLSpanElement;
+const err = document.querySelector('.errMsg') as HTMLSpanElement;
+
+// Initialize todos array
+let todos: Todo[] = [];
+
+// Handle form submission
+const handleSubmit = (e: Event) => {
+  e.preventDefault();
+
+  const newTask = inputTodo.value.trim();
+
+  // Validate input data
+  if (newTask === '') {
+    // Display an error message or take appropriate action
+    console.error('Error: Task cannot be empty');
+    return; // return early if validation fails
+
+    const err = document.createElement('err');
+    err.innerHTML = `
+    <p>Error: Task cannot be empty</p>
+    `;
+  }
+
+  // If task is not empty, add it to todos
+  if (newTask !== '') {
+    const todo: Todo = {
+      id: Date.now(),
+      task: newTask,
+      completed: false // Set completed property to false by default
+    };
+    todos.push(todo);
+    saveTodos();
+    appendTodoToDOM(todo);
+    inputTodo.value = '';
+    updateTodoCount();
+  }
+};
+
+// Handle delete task
+const handleDelete = (id: number) => {
+  todos = todos.filter(todo => todo.id !== id);
+  saveTodos();
+  updateTodoList();
+  updateTodoCount();
+};
+
+// Handle edit task
+const handleEdit = (id: number, task: string) => {
+  const newTask = prompt('Edit task:', task);
+  if (newTask !== null && newTask !== '') {
+    const todoIndex = todos.findIndex(todo => todo.id === id);
+    if (todoIndex !== -1) {
+      todos[todoIndex].task = newTask;
+      saveTodos();
+      updateTodoList();
+    }
+  }
+};
+
+// Handle toggle completed status
+const handleToggleCompleted = (id: number) => {
+  const todoIndex = todos.findIndex(todo => todo.id === id);
+  if (todoIndex !== -1) {
+    todos[todoIndex].completed = !todos[todoIndex].completed;
+    saveTodos();
+    updateTodoList();
+  }
+};
+
+// Append todo to DOM
+const appendTodoToDOM = (todo: Todo) => {
+  const table = document.createElement('table');
+  table.innerHTML = `
+    <table style="width: 100%; border-collapse: collapse; border: 1px solid #ddd;">
+      <tbody>
+        <td>
+          <input type="checkbox" class="checkbox" ${todo.completed ? 'checked' : ''}>
+          <span class="task ${todo.completed ? 'completed' : ''}">${todo.task}</span>
+        </td>
+        <td>
+          <button class="btn-delete" style="background-color: rgb(241, 97, 97); 
+                                            text-align: center; display: inline-block; border-color: black;">Delete</button>
+        </td>
+        <td>
+          <button class="btn-edit" style="background-color: rgb(8, 248, 8); 
+                                          border-color: black;">Edit</button>
+        </td>
+      </tbody>
+    </table> 
+  `;
+
+  const checkbox = table.querySelector('.checkbox') as HTMLInputElement;
+  const btnDelete = table.querySelector('.btn-delete') as HTMLButtonElement;
+  const btnEdit = table.querySelector('.btn-edit') as HTMLButtonElement;
+
+  // Add event listeners for checkbox, delete button, and edit button
+  checkbox.addEventListener('change', () => handleToggleCompleted(todo.id));
+  btnDelete.addEventListener('click', () => handleDelete(todo.id));
+  btnEdit.addEventListener('click', () => handleEdit(todo.id, todo.task));
+
+  todoList.appendChild(table);
+};
+
+// Update todo list in DOM
+const updateTodoList = () => {
+  todoList.innerHTML = '';
+  todos.forEach(todo => appendTodoToDOM(todo));
+};
+
+// Function to update the todo count in the DOM
+function updateTodoCount() {
+  const count = todos.length;
+  todoCount.textContent = `${count} task${count !== 1 ? 's' : ''} remaining`;
+}
+
+// Load todos from localStorage
+const loadTodos = () => {
+  const storedTodos = localStorage.getItem('todos');
+  todos = storedTodos ? JSON.parse(storedTodos) : [];
+  updateTodoList();
+};
+
+// Save todos to localStorage
+const saveTodos = () => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+// Add form submit event listener
+formTodo.addEventListener('submit', handleSubmit);
+  
+// Load todos from localStorage on page load
+  loadTodos();
+
+function updateTodoCount1() {
+  throw new Error("Function not implemented.");
+}
